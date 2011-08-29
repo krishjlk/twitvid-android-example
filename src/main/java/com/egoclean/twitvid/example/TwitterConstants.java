63992f947f7fba90c3e4d1b92e0836377934552a
Cristian <casidiablo@elhacker.net>
2011-08-29 10:28:16 -0500

    rm tw

--- src/main/java/com/egoclean/twitvid/example/TwitterConstants.java
+++ src/main/java/com/egoclean/twitvid/example/TwitterConstants.java
@@ -1,8 +1,8 @@
 package com.egoclean.twitvid.example;
 
 final class TwitterConstants {
-    public static final String CONSUMER_KEY = "n3ELD1K3bqPjSoUhVFlw";
-    public static final String CONSUMER_SECRET = "Jy9JSU8xcJ18CR7SV6KA6KEbYSn2YkMlr5rA1OiweI";
+    public static final String CONSUMER_KEY = null;
+    public static final String CONSUMER_SECRET = null;
 
     public final static String CALLBACK_SCHEME = "x-example-oauth-twitter";
     public final static String CALLBACK_URL = CALLBACK_SCHEME + "://callback";
